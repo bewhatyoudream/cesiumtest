91657699c19c5f091a2925c5844dfb16c8910813
Sun <371360082@qq.com>
2024-05-09 14:44:43 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { onMounted } from 'vue'
+  import * as Cesium from 'cesium'
+
+  onMounted(() => {
+    const map = new Cesium.Viewer('cesiumContainer')
+  })
+</script>
+
+<template>
+  <div id="cesiumContainer"/>
+
+</template>
+
+<style scoped>
+  #cesiumContainer {
+    width: 100%;
+    height: 100%;
+    max-height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
+</style>
